8ea20252e59e64ca48cd3d84dad28917223bfe4f
Lazy Monad <nopfault@Lazys-MacBook-Air.local>
2023-02-10 15:19:57 +0200

    Add gx cmd to follow the link in browser

--- lua/nopfault/keys.lua
+++ lua/nopfault/keys.lua
@@ -10,3 +10,11 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+if vim.fn.has("mac") == 1 then
+	vim.api.nvim_set_keymap("", "gx", '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>', {})
+elseif vim.fn.has("unix") == 1 then
+	vim.api.nvim_set_keymap("", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', {})
+else
+	vim.api.nvim_set_keymap[''].gx = {'<Cmd>lua print("Error: gx is not supported on this OS!")<CR>'}
+end
